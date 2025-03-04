dfb71a03e5d2960e5a0ceb699b517beb75730bd3
subhansu7 <sarangisubhansu7@gmail.com>
2025-03-04 08:47:09 +0530

    Update main.tf
    corrected key name parameter

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "dev" {
     ami = "ami-00db8dadb36c9815e"
     instance_type = "t2.nano"
-    key-name = "key-n-virginia"
+    key_name = "key-n-virginia"
     tags = {
       Name = "dev123"
     }
