54a3f5a0e62350807c86a51b745f05e1cbb916b1
subhansu7 <sarangisubhansu7@gmail.com>
2025-03-04 05:59:03 +0530

    Create main.tf
    Create first file

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "dev" {
+    ami = "ami-00db8dadb36c9815e"
+    instance_type = "t2.nano"
+    tags = {
+      Name = "dev123"
+    }
+}
