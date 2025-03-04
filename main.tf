36b985e30822e0ac7fbb4c5e7d8b14af0623adeb
subhansu7 <sarangisubhansu7@gmail.com>
2025-03-04 06:15:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "dev" {
     ami = "ami-00db8dadb36c9815e"
     instance_type = "t2.nano"
+    key-name = "key-n-virginia"
     tags = {
       Name = "dev123"
     }
